d25aebdac04695dff68296647764e8a6dccafda8
Sébastien <merize94@gmail.com>
2017-11-30 00:31:40 +0100

    dvs: comment app2

--- template-mobile-web/setup-rn/LibProject/index.js
+++ template-mobile-web/setup-rn/LibProject/index.js
@@ -1,7 +1,7 @@
 
 import { AppRegistry } from 'react-native';
-// import App from './App';
- import App2 from './App2';
+import App from './App';
+ //import App2 from './App2';
 //import App from './zComponents/App.js'
 
 AppRegistry.registerComponent('LibProject', () => App2);
