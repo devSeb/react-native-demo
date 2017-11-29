93be9fb2c84e4b0d0369361084cc300b0cc72e4f
Sébastien <merize94@gmail.com>
2017-11-30 00:30:06 +0100

    dvs: fix config gradle

--- template-mobile-web/setup-rn/LibProject/index.js
+++ template-mobile-web/setup-rn/LibProject/index.js
@@ -1,8 +1,10 @@
-import { AppRegistry } from 'react-native';
-//import App from './App';
-import App from './zComponents/App.js'
 
-AppRegistry.registerComponent('LibProject', () => App);
+import { AppRegistry } from 'react-native';
+// import App from './App';
+ import App2 from './App2';
+//import App from './zComponents/App.js'
+
+AppRegistry.registerComponent('LibProject', () => App2);
 
 if (window.document) {
     AppRegistry.runApplication("LibProject", {
