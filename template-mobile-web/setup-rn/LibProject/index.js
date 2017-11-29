c6f6e7cea18c0faabae726b92d789b861871931d
Sébastien <merize94@gmail.com>
2017-11-29 23:30:01 +0100

    dvs:  camera works

--- template-mobile-web/setup-rn/LibProject/index.js
+++ template-mobile-web/setup-rn/LibProject/index.js
@@ -1,6 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-AppRegistry.registerComponent('LibProject', () => App);
+import App2 from './App2';
+AppRegistry.registerComponent('LibProject', () => App2);
 
 if (window.document) {
     AppRegistry.runApplication("LibProject", {
